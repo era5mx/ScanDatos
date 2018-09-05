1cea02b362403e8fbd1b3bf955655da5a3e4060b
David Rengifo <david.rengifo@anglobal.com>
2018-09-05 18:46:04 -0500

    Se incorporo el uso de java.util.logging.Logger y se mejoran los mensajes.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ScanDatos
-Clase para realizar el scan de un directorio y contar las lineas de los archivos .txt, .TXT contenidos en este
+Clase para realizar el scan de un directorio y contar las lineas de los archivos .txt, .TXT, .log o .LOG contenidos en este
 
 Ver más en: <a href="https://davidrengifo.wordpress.com/2014/06/17/realizar-el-scan-de-un-directorio-y-contar-las-lineas-de-los-archivos-de-texto-contenidos-en-este-mediante-java/">https://davidrengifo.wordpress.com/2014/06/17/realizar-el-scan-de-un-directorio-y-contar-las-lineas-de-los-archivos-de-texto-contenidos-en-este-mediante-java/</a>
--- ScanDatos.java
+++ ScanDatos.java
@@ -1,28 +1,34 @@
 /**
  * Clase para realizar el scan de un directorio
- * y contar las lineas de los archivos .txt, .TXT contenidos en este
+ * y contar las lineas de los archivos .txt, .TXT, .log o .LOG contenidos en este
  */
-package net.puenteg.extend;
+
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * Lee el directorio indicado y obtiene la lista de archivos luego cuenta las
  * lineas de los archivos .txt o .TXT y finalmente presenta el total de
  * registros procesados
  *
- * @author david.rengifo
+ * @author <a href="mailto:david@rengifo.mx">david.rengifo</a>
  */
 public class ScanDatos {
+	
 	/**
 	 * El path absoluto donde estan los archivos, se debe escapar con \\ y no
 	 * colocar \\ al final
 	 */
-	private static final String DATOS_DIR = "{AQUI_VA_EL_PATH_DONDE_ESTAN_LOS_ARCHIVOS}";
+	//private static final String DATOS_DIR = "{AQUI_VA_EL_PATH_DONDE_ESTAN_LOS_ARCHIVOS}";
+	private static final String DATOS_DIR = "C:\\Users\\alexander\\Downloads\\icons";
+	
+	/** Logger */
+	private static Logger logger = Logger.getLogger(ScanDatos.class.getName());
 
 	/**
 	 * Constructor
@@ -34,41 +40,43 @@ public class ScanDatos {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		logger.info("Ejecutando ScanDatos sobre el directorio [" + DATOS_DIR + "]\n");
+		
 		File dir = new File(DATOS_DIR);
 		String[] ficheros = dir.list();
 		long totalReg = 0;
 		long ignoreFiles = 0;
-		if (ficheros == null)
-			System.out.println("No hay ficheros en el directorio especificado");
-		else {
-			System.out.println("Hay " + ficheros.length
-					+ " archivos en el directorio Datos\n");
+		if (ficheros == null) {
+			logger.info("No se encontro el directorio [" + DATOS_DIR + "], o no hay archivos en el");
+		} else {
+			logger.info("Hay " + ficheros.length + " archivos en el directorio [" + DATOS_DIR + "] \n");
 			for (int i = 0; i < ficheros.length; i++) {
 				String nameFile = ficheros[i];
 				if (nameFile.endsWith("txt") || nameFile.endsWith("TXT")
 						|| nameFile.endsWith(".log")
 						|| nameFile.endsWith(".LOG")) {
-					System.out.print("El archivo [" + nameFile);
+					logger.info("El archivo [" + nameFile);
 					long linesFile = contarLineas(nameFile);
 					if (linesFile > 0) {
 						totalReg = totalReg + linesFile;
 						System.out.println("] tiene " + linesFile + " lineas");
+						logger.info("] tiene " + linesFile + " lineas");
 					} else {
 						System.out.println("] esta vacio.");
+						logger.info("] esta vacio.");
 					}
 				} else {
 					ignoreFiles++;
 				}
 			}
 			if (totalReg > 0) {
-				System.out.println("\nSe procesaron " + totalReg
-						+ " registros en total.");
+				logger.info("Se procesaron " + totalReg + " registros en total.\n");
 			} else {
-				System.out.println("\nNo se procesaron registros.");
+				logger.info("No se procesaron registros.\n");
 			}
 			if (ignoreFiles > 0) {
-				System.out.println("\nSe ignoraron " + ignoreFiles
-						+ " archivos del directorio Datos.");
+				logger.info("Se ignoraron " + ignoreFiles + " archivos del directorio [" + DATOS_DIR + "].");
 			}
 		}
 	}
@@ -92,24 +100,25 @@ public class ScanDatos {
 				lNumeroLineas++;
 			}
 		} catch (FileNotFoundException e) {
-			System.out.println(e.getCause());
-			e.printStackTrace();
+			printLog(e);
 		} catch (IOException e) {
-			System.out.println(e.getCause());
-			e.printStackTrace();
+			printLog(e);
 		} finally {
 			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
+				if (br != null) { br.close(); }
+				if (fr != null) { fr.close(); }
 			} catch (IOException e) {
-				System.out.println(e.getCause());
-				e.printStackTrace();
+				printLog(e);
 			}
 		}
 		return lNumeroLineas;
 	}
+	
+	/** Print log */
+	private static void printLog(Exception e) {
+		logger.severe(e.getClass().getTypeName());
+		logger.severe(e.getMessage());
+		logger.severe(e.getCause().getMessage());
+	}
+	
 }
